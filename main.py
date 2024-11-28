4713d3bf57924cd45c4cb125ebb21e077198a0be
SominZex <sominzex21@gmail.com>
2024-11-28 15:29:37 +0530

    Minor Fixes

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import streamlit as st
 import subprocess
 import os
 
-# Set the page layout to wide
-st.set_page_config(page_title="Unified Analysis Dashboard", layout="wide")
+st.set_page_config(page_title="The New Shop:Data Factory", layout="wide")
 
-# Apply custom CSS to increase content width
 st.markdown(
     """
     <style>
@@ -17,16 +15,14 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-st.markdown("<h1 style='text-align: center;'>Unified Analysis Dashboard</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>TNS Unified Analysis Dashboard</h1>", unsafe_allow_html=True)
 
 
-# Use a dropdown menu (selectbox) instead of buttons
 analysis_type = st.selectbox(
     "Choose an analysis type", 
     ["Select", "Product Analysis", "Category Analysis", "Store Analysis", "Brand Analysis"]
 )
 
-# Run corresponding analysis based on the selected option
 if analysis_type == "Product Analysis":
     st.subheader("Running Product Analysis (Switch the browser tab)")
     subprocess.run(["streamlit", "run", os.path.join("product_analysis", "main.py")])
@@ -44,4 +40,4 @@ elif analysis_type == "Brand Analysis":
     subprocess.run(["streamlit", "run", os.path.join("brand_analysis", "main.py")])
 
 else:
-    st.warning("Please select an analysis type to proceed.")
+    st.warning("Select an analysis type to proceed.")
